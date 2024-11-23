bd757d88a42d0728b20253da9c8718ab9e3ecd43
Domisie543 <dbog1998@gmail.com>
2024-11-23 12:56:06 +0000

    Tolerance change by Polish

--- PizzaScriptProgression1.py
+++ PizzaScriptProgression1.py
@@ -7,8 +7,8 @@ import time
 import keyboard
 import threading
 
-#################################################################################################################################################################################################################################
-#################################################################################################################################################################################################################################
+############################################# INTERRUPTS ####################################################################################################################################################################################
+############################################# INTERRUPTS ###############################################################################################################################################################################
 running = False
 kill = False
 
@@ -27,17 +27,32 @@ def handle_kill():
     print("killing program")
     kill = True
 
-#################################################################################################################################################################################################################################
-#################################################################################################################################################################################################################################
+############################################# GREEN BUTTON PRESSING #################################################################################################################################################################################################################################
+############################################# GREEN BUTTON PRESSING ####################################################################################################################################################################################
 noBut = 0
 time.sleep(1)
-def find_specific_color(image, target_color):
+def find_specific_color(image, target_color, tolerance=10):
     # Convert the target color from RGB to BGR (OpenCV uses BGR format)
     target_color_bgr = target_color[::-1]
-    # Convert the screenshot image to a numpy array
+    
+    # Define the lower and upper bounds with the tolerance
+    lower_bound = np.array([
+        max(0, target_color_bgr[0] - tolerance),
+        max(0, target_color_bgr[1] - tolerance),
+        max(0, target_color_bgr[2] - tolerance)
+    ])
+    upper_bound = np.array([
+        min(255, target_color_bgr[0] + tolerance),
+        min(255, target_color_bgr[1] + tolerance),
+        min(255, target_color_bgr[2] + tolerance)
+    ])
+    
+    # Convert the screenshot image to BGR
     bgr_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    # Find the exact match for the target color
-    mask = cv2.inRange(bgr_image, np.array(target_color_bgr), np.array(target_color_bgr))
+    
+    # Find pixels within the color range
+    mask = cv2.inRange(bgr_image, lower_bound, upper_bound)
+    
     # Find contours for the color
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
@@ -48,57 +63,37 @@ def find_specific_color(image, target_color):
             return x + w // 2, y + h // 2  # Return the center coordinates
     return None
 
+
 def main():
     global noBut
     global running
     keyboard.on_press_key('e', lambda _: handle_start())
     keyboard.on_press_key('q', lambda _: handle_stop())
     keyboard.on_press_key('0', lambda _: handle_kill())
-    target_color = (82, 255, 0)  # Replace with your specific green color
+    target_color = (82, 255, 0) 
     while True:
         if kill == True:
             break
         while running == True:
             while noBut <= 20:
-                # Capture the screen
                 screenshot = pyautogui.screenshot()
-                # Convert screenshot to numpy array
                 frame = np.array(screenshot)
-                # Find the specific green button
                 green_button_pos = find_specific_color(frame, target_color)
                 if green_button_pos:
-                    # Click the green button
                     pyautogui.click(green_button_pos)
                     time.sleep(0.5)
                 else:
                     print("Green button not found.")
                     noBut += 1   
-        noBut = 0
+        noBut = 0                                               
     
 
 
 #################################################################################################################################################################################################################################
 #################################################################################################################################################################################################################################
 
-############################################# PIZZA COMPARE ######################################################################################################################################################################
-############################################# PIZZA COMPARE ######################################################################################################################################################################
-# resolution = 1920/1080
-
-#def pizzatopping(what_screen_sees):
-    #screen_image = cv2.imread("Screen.PNG", cv2.IMREAD_UNCHANGED)
-    #tomato_pizza = cv2.imread("Tomato.PNG", cv2.IMREAD_UNCHANGED)
-    #artichoke_pizza = cv2.imread("Artichoke.PNG", cv2.IMREAD_UNCHANGED)
-#
-  #  result = cv2.matchTemplate(screen_image, artichoke_pizza, cv2.TM_CCOEFF_NORMED)
-  #  cv2.imshow('Result', result)
-  #  cv2.waitKey()
-
-
-
-
-
-############################################# ACTUAL SCRIPT ####################################################################################################################################################################################
-############################################# ACTUAL SCRIPT ####################################################################################################################################################################################
+############################################# CHECKING PIZZA TYPE ####################################################################################################################################################################################
+############################################# CHECKING PIZZA TYPE ####################################################################################################################################################################################
 
 tomato_pizza = cv2.imread("C:\\python\\Automation\\Tomato.PNG", cv2.IMREAD_COLOR)
 artichoke_pizza = cv2.imread("C:\\python\\Automation\\Artichoke.PNG", cv2.IMREAD_COLOR)
@@ -118,7 +113,7 @@ M_Olv_Pizza = 0
 
 def checkpizzatypes(screenimagescreenshot):
 
-    screen_image = cv2.imread("C:\\python\\Automation\\Screen.PNG", cv2.IMREAD_UNCHANGED) ####CHANGE IN FUTURE
+    screen_image = cv2.imread("C:\\python\\Automation\\Screen.PNG", cv2.IMREAD_UNCHANGED) ####CHANGE IN FUTURE FOR SCREENSHOT
 
 
     Tomresult = cv2.matchTemplate(screen_image, tomato_pizza, cv2.TM_CCOEFF_NORMED)
@@ -159,10 +154,14 @@ def checkpizzatypes(screenimagescreenshot):
         pyautogui.click(maxOlv_loc)
 
 
+##############################################################################################################################################################################################################################################################################
+##############################################################################################################################################################################################################################################################################
+
+
+############################################# SELECTING PIZZA TOPPING #################################################################################################################################################################################################################################
+############################################# SELECTING PIZZA TOPPING #################################################################################################################################################################################################################################
+
+main()
+
+#def PickPizza():
     
-    pyautogui.click(max_loc)
-
-    cv2.imshow('Result', result)
-    cv2.waitKey()
-
-
